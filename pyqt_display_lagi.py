0d71e6e17c2fe9ea112ec3acd6a6cf70818e9798
Aditya Anandita Dharma Putra <97426366+adityanandita@users.noreply.github.com>
2023-11-09 11:21:24 +0700

    Update pyqt_display_lagi.py

--- pyqt_display_lagi.py
+++ pyqt_display_lagi.py
@@ -1,8 +1,9 @@
 import sys
+import os
 import random
 import time
-from PyQt5.QtWidgets import QHBoxLayout, QApplication, QMainWindow, QWidget, QLabel, QPushButton, QComboBox, QVBoxLayout, QGridLayout, QTreeWidget, QTreeWidgetItem, QMessageBox
-from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QHeaderView, QAbstractScrollArea, QTableView, QTabBar, QHBoxLayout, QApplication, QMainWindow, QWidget, QLabel, QPushButton, QComboBox, QVBoxLayout, QGridLayout, QTreeWidget, QTreeWidgetItem, QMessageBox
+from PyQt5.QtCore import QTimer, Qt, QThread, pyqtSignal
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from pynput import keyboard
@@ -15,18 +16,22 @@ from services.save_service import save_service
 from services.upload_service import upload_service
 
 
+# Direktori untuk menyimpan data hasil identifikasi
+directory = r'c:\Users\user\OneDrive\Dokumen'
+
+
 class PCRIdentificationProcess(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PCR Identification Process")
-        self.setGeometry(100, 100, 800, 480)
+        self.setGeometry(512, 300, 1024, 600)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
         self.profile_combobox = QComboBox()
         self.profile_combobox.addItems(
-            [str(key) for key in profile_PCR.keys()])
+            [profile_PCR[key]['nama_profile'] for key in profile_PCR.keys()])
 
         self.profile_buttons = []
         for key in profile_PCR.keys():
@@ -34,50 +39,47 @@ class PCRIdentificationProcess(QMainWindow):
             self.profile_buttons.append(button)
 
         self.profile_grid = QGridLayout()
-        self.profile_grid.setHorizontalSpacing(10)
-        self.profile_grid.setVerticalSpacing(10)
+        self.profile_grid.setHorizontalSpacing(15)
+        self.profile_grid.setVerticalSpacing(15)
         self.profile_grid.addWidget(QLabel("PCR Identification Process", alignment=QtCore.Qt.AlignCenter,
                                            font=QtGui.QFont("Sanserif", 20, QtGui.QFont.Bold)))
         self.profile_grid.addWidget(
-            QLabel("Pilih Profile PCR yang Akan Digunakan: "), 1, 0)
-
-        # row = 1
-        # col = 0
-        # for button in self.profile_buttons:
-        #     self.profile_grid.addWidget(button, row, col)
-        #     col += 1
-        #     if col == 3:
-        #         col = 0
-        #         row += 1
-
-        # if len(self.profile_buttons) % 3 != 0:
-        #     spacer = QWidget()
-        #     self.profile_grid.addWidget(spacer, row, col, 1, 2)
-        # self.profile_grid.clicked.connect(self.show_selected_profile)
+            QLabel("Pilih Profile PCR yang Akan Digunakan: "))
 
         self.start_button = QPushButton("Start")
         self.start_button.clicked.connect(self.switch_to_process)
 
+        self.history_button = QPushButton("History")
+        self.history_button.clicked.connect(self.switch_to_histories)
+
         self.exit_button = QPushButton("Exit")
         self.exit_button.clicked.connect(self.close)
 
+        self.grid_layout = QHBoxLayout()
+        self.grid_layout.addWidget(self.profile_combobox)
+        self.grid_layout.addWidget(self.start_button)
+        self.grid_layout.addWidget(self.history_button)
+        self.grid_layout.addWidget(self.exit_button)
+
         self.layout = QVBoxLayout()
         self.layout.addLayout(self.profile_grid)
-        self.layout.addWidget(self.profile_combobox)
-        self.layout.addWidget(self.start_button)
-        self.layout.addWidget(self.exit_button)
+        self.layout.addLayout(self.grid_layout)
 
         self.central_widget.setLayout(self.layout)
         self.selected_profile = None
 
     def switch_to_process(self):
-        self.selected_profile = self.profile_combobox.currentText()
+        self.selected_profile = self.profile_combobox.currentIndex() + 1
         if self.selected_profile:
             self.process_view = ProcessView(self.selected_profile)
             self.setCentralWidget(self.process_view)
         else:
             QMessageBox.critical(
                 self, "Error", "Please select a PCR profile first.")
+
+    def switch_to_histories(self):
+        self.histories_view = HistoriesView()
+        self.setCentralWidget(self.histories_view)
 
     def show_selected_profile(self, button):
         self.selected_profile = int(button.text())
@@ -89,32 +91,51 @@ class ProcessView(QWidget):
         self.input_numpad = int(input_numpad)
         self.process_running = False
         self.chart_thread = None
+        self.table_thread = None
         self.process_thread = None
-        self.chart_timer = QTimer(self)
+        self.timer = QTimer(self)
+        self.data_suhu = []
+        self.data_intensity_sampel_1 = []
+        self.data_intensity_sampel_2 = []
+        self.data_intensity_control_pos = []
+        self.data_intensity_control_neg = []
+        self.all_data = {}
+        # Lock untuk sinkronisasi akses data_suhu dan data_intensity
+        self.data_lock = threading.Lock()
         self.initUI()
 
     def initUI(self):
-        self.fig, self.ax = plt.subplots(figsize=(6, 4))
+        self.fig, self.ax = plt.subplots(figsize=(7, 4))
         data_pcr = profile_PCR.get(self.input_numpad)
-        self.ax.set_xlim(0, len(data_pcr))
+        self.ax.set_xlim(0, data_pcr["jumlah_cycle"]+1)
         self.ax.set_ylim(0, 100)
         self.canvas = FigureCanvas(self.fig)
         self.canvas.draw()
 
         self.table = QTreeWidget()
         self.table.setHeaderLabels(["Component", "Value"])
-        self.table.setColumnWidth(0, 100)
-        self.table.setColumnWidth(1, 80)
+        self.table.setColumnWidth(0, 170)
+        self.table.setColumnWidth(1, 50)
 
         # Buat objek QTreeWidgetItem
         self.cycle_item = QTreeWidgetItem(["Cycle", "0"])
         self.suhu_item = QTreeWidgetItem(["Suhu", "0"])
-        self.light_item = QTreeWidgetItem(["Intensity", "0"])
+        self.intensity_control_pos = QTreeWidgetItem(
+            ["Intensity Control Pos", "0"])
+        self.intensity_control_neg = QTreeWidgetItem(
+            ["Intensity Control Neg", "0"])
+        self.intensity_sampel_1_item = QTreeWidgetItem(
+            ["Intensity Sample 1", "0"])
+        self.intensity_sampel_2_item = QTreeWidgetItem(
+            ["Intensity Sample 2", "0"])
 
         # Tambahkan objek QTreeWidgetItem ke objek QTreeWidget
         self.table.addTopLevelItem(self.cycle_item)
         self.table.addTopLevelItem(self.suhu_item)
-        self.table.addTopLevelItem(self.light_item)
+        self.table.addTopLevelItem(self.intensity_control_pos)
+        self.table.addTopLevelItem(self.intensity_control_neg)
+        self.table.addTopLevelItem(self.intensity_sampel_1_item)
+        self.table.addTopLevelItem(self.intensity_sampel_2_item)
 
         self.simulate_button = QPushButton("Simulate Process")
         self.simulate_button.clicked.connect(self.simulate_process)
@@ -141,16 +162,43 @@ class ProcessView(QWidget):
 
         self.setLayout(self.layout_main)
 
-    def simulate_process(self):
+    def start_simulation(self):
         profile = profile_PCR.get(self.input_numpad)
-        num_cycle = 0
+        if profile:
+            cycle_times = profile["jumlah_cycle"]
+            if self.current_cycle < cycle_times:
+                self.process_thread = threading.Thread(
+                    target=self.run_simulation)
+                self.process_thread.start()
+                self.chart_thread = threading.Thread(target=self.update_chart)
+                self.chart_thread.start()
+                self.table_thread = threading.Thread(target=self.update_table)
+                self.table_thread.start()
+                self.stop_thread = threading.Thread(target=self.stop_process)
+                self.stop_thread.start()
+
+                # 100000 adalah faktor konversi dari detik ke milidetik
+                cycle_time = (sum(profile['waktu']) * 1000000)
+                self.timer.setInterval(cycle_time)
+                self.timer.start(cycle_time)
+            else:
+                self.all_data = {
+                    "Sample 1": self.data_intensity_sampel_1,
+                    "Sample 2": self.data_intensity_sampel_2,
+                    "Control Positive": self.data_intensity_control_pos,
+                    "Control Negative": self.data_intensity_control_neg
+                }
+                self.stop_process()
+                self.stop_thread.join()
+                save_service("txt", self.all_data, directory)
+        else:
+            QMessageBox.critical(self, "Error", "No profile selected.")
+
+    def simulate_process(self):
         if not self.process_running:
             self.process_running = True
-            # self.chart_timer.timeout.connect(self.update_chart)
-            self.chart_thread = threading.Thread(target=self.update_chart)
-            self.chart_thread.start()
-            self.process_thread = threading.Thread(target=self.run_simulation)
-            self.process_thread.start()
+            self.current_cycle = 0
+            self.start_simulation()
         else:
             QMessageBox.critical(
                 self, "Error", "Process is already running.")
@@ -158,53 +206,115 @@ class ProcessView(QWidget):
     def run_simulation(self):
         profile = profile_PCR.get(self.input_numpad)
         if profile:
-            while self.process_running:
-                self.chart_timer.start(1000)  # Update chart every 1 seconds
+            with self.data_lock:
+                self.data_suhu.append(random.randint(30, 90))
+                self.data_intensity_sampel_1.append(
+                    random.randint(5, 80))
+                self.data_intensity_sampel_2.append(
+                    random.randint(5, 80))
+                self.data_intensity_control_pos.append(
+                    random.randint(50, 100))
+                self.data_intensity_control_neg.append(
+                    random.randint(10, 40))
+                self.current_cycle += 1
+            self.update_table()
+            self.update_chart()
+            self.start_simulation()
         else:
             QMessageBox.critical(self, "Error", "No profile selected.")
 
     def stop_process(self):
         if self.process_running:
             self.process_running = False
-            self.chart_timer.stop()
             self.chart_thread.join()
+            self.table_thread.join()
             self.process_thread.join()
+            self.timer.stop()
 
     def update_chart(self):
         profile = profile_PCR.get(self.input_numpad)
-        data_suhu = []
-        data_intensity = []
         if profile:
-            while self.process_running and len(data_suhu) < profile["jumlah_cycle"]:
-                data_suhu.append(random.randint(30, 90))
-                data_intensity.append(random.randint(0, 80))
+            with self.data_lock:
                 self.ax.clear()
                 self.ax.plot(
-                    [(i+1) for i in range(len(data_intensity))], data_intensity)
+                    [i+1 for i in range(len(self.data_intensity_control_pos))], self.data_intensity_control_pos)
                 self.ax.plot(
-                    [(i+1) for i in range(len(data_suhu))], data_suhu)
-                # tambahkan legend
-                self.ax.legend(["Sampel 1", "Sampel 2"])
-                # tambahkan judul grafik
+                    [i+1 for i in range(len(self.data_intensity_control_neg))], self.data_intensity_control_neg)
+                self.ax.plot(
+                    [i+1 for i in range(len(self.data_intensity_sampel_1))], self.data_intensity_sampel_1)
+                self.ax.plot(
+                    [i+1 for i in range(len(self.data_intensity_sampel_2))], self.data_intensity_sampel_2)
+                self.ax.legend(
+                    ["Control Positive", "Control Negative", "Sampel 1", "Sampel 2"], loc='upper right')
                 self.ax.set_title("PCR Graphic Result")
-                self.ax.set_xlabel("Jumlah Cycle")
+                self.ax.set_xlabel("Number of Cycle")
                 self.ax.set_xlim(0, profile["jumlah_cycle"]+1)
                 self.ax.set_ylim(0, 100)
-                self.ax.set_ylabel("Pembacaan Lux")
-                # Ubah nilai suhu di tabel menjadi nilai data terakhir
-                self.suhu_item.setText(1, str(data_suhu[-1]))  # Dummy
-                self.cycle_item.setText(1, str(len(data_suhu)))  # Udah Bener
-                self.light_item.setText(1, str(data_intensity[-1]))  # Dummy
+                self.ax.set_ylabel("Lux Levels")
                 self.canvas.draw()
-            # jika selesai, jalankan fungsi stop_process
-            self.stop_process()
+        else:
+            QMessageBox.critical(self, "Error", "No profile selected.")
+
+    def update_table(self):
+        profile = profile_PCR.get(self.input_numpad)
+        if profile:
+            with self.data_lock:
+                self.cycle_item.setText(1, str(len(self.data_suhu)))
+                self.suhu_item.setText(1, str(self.data_suhu[-1]))
+                self.intensity_control_pos.setText(
+                    1, str(self.data_intensity_control_pos[-1]))
+                self.intensity_control_neg.setText(
+                    1, str(self.data_intensity_control_neg[-1]))
+                self.intensity_sampel_1_item.setText(
+                    1, str(self.data_intensity_sampel_1[-1]))
+                self.intensity_sampel_2_item.setText(
+                    1, str(self.data_intensity_sampel_2[-1]))
         else:
             QMessageBox.critical(self, "Error", "No profile selected.")
 
     def switch_to_standby(self):
-        self.stop_process()
         self.standby_view = PCRIdentificationProcess()
         window.setCentralWidget(self.standby_view)
+
+
+class HistoriesView(QAbstractScrollArea):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.table = QTableView()
+        self.model = self.initModel()
+        self.table.setModel(self.model)
+
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.Stretch)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.table)
+        self.setLayout(self.layout)
+
+    def initModel(self):
+        model = QtGui.QStandardItemModel(self)
+        model.setHorizontalHeaderLabels(["No.", "Date", "Result", "Action"])
+
+        # Membaca file csv dan png di folder Hasil pada direktori utama
+        # dan menambahkan data ke dalam tabel
+        path = r'c:\Users\user\OneDrive\Dokumen'
+        files = os.listdir(path)
+
+        for i, file in enumerate(files):
+            if file.endswith(".txt"):
+                row = []
+                row.append(QtGui.QStandardItem(str(i+1)))
+                # row.append(QtGui.QStandardItem(file[:-4]))
+                row.append(QtGui.QStandardItem(
+                    os.path.getctime(os.path.join(path, file))))
+                row.append(QtGui.QStandardItem("5 Positive"))
+                row.append(QtGui.QStandardItem("View"))
+                model.appendRow(row)
+
+        return model
 
 
 def on_press(key):
